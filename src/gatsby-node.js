e03c5582bef41c86425e1cc9e44b106fd7bcd91a
Justin Hall <justin.r.hall@gmail.com>
2020-11-19 12:34:38 -0700

    feat: add plugin options validation (#63)
    * feat: add plugin options validation
    
    Note: plugin options validation requires gatsby v2.25.0 or later.
    
    * test: add plugin options validation tests

---
+++ src/gatsby-node.js
@@ -0,0 +1,16 @@
+exports.pluginOptionsSchema = ({ Joi }) => {
+  return Joi.object({
+    classNameDark: Joi.string()
+      .default('dark-mode')
+      .description('CSS class name applied in dark mode'),
+    classNameLight: Joi.string()
+      .default('light-mode')
+      .description('CSS class name applied in light mode'),
+    storageKey: Joi.string()
+      .default('darkMode')
+      .description('localStorage key used to persist mode'),
+    minify: Joi.boolean()
+      .default(true)
+      .description('toggle minification of the injected script'),
+  });
+};

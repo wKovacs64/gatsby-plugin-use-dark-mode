9f70d03f75c836de6bf46307443af2d73d4b841e
Justin Hall <justin.r.hall@gmail.com>
2022-07-22 11:46:25 -0600

    test: convert vitest config to TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['plugin:wkovacs64/react', 'plugin:wkovacs64/jest', 'prettier'],
+  extends: [
+    'plugin:wkovacs64/react',
+    'plugin:wkovacs64/jest',
+    'plugin:wkovacs64/typescript',
+    'prettier',
+  ],
   rules: {
     // doesn't work with vitest as it relies on jest version detection
     'jest/no-deprecated-functions': 'off',

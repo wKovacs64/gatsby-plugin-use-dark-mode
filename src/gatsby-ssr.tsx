4032862f043e086b880177d38b87465c22bcd60b
Justin Hall <justin.r.hall@gmail.com>
2024-11-24 12:54:02 -0700

    style: reformat

--- src/gatsby-ssr.tsx
+++ src/gatsby-ssr.tsx
@@ -6,7 +6,6 @@ export const onRenderBody: GatsbySSR['onRenderBody'] = ({ setPreBodyComponents }
     <script
       key="gatsby-plugin-use-dark-mode"
       id="gatsby-plugin-use-dark-mode"
-       
       dangerouslySetInnerHTML={{
         // populated in gatsby-node via Webpack's DefinePlugin
         __html: String(process.env.GATSBY_PLUGIN_USE_DARK_MODE_NO_FLASH_SCRIPT),
